7c5a29b3a855371388c03653aedfd5a102046666
ididani <ididani20@epoka.edu.al>
2025-02-28 16:45:23 +0100

    server.js updated

--- server/server.js
+++ server/server.js
@@ -21,26 +21,35 @@ app.use(
   cors({
     origin: "https://designeral.netlify.app",
     credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+
 app.use(express.json({ limit: "1000mb", extended: true }));
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
+SESSION_SECRET = "your_secure_secret";
+
 app.use(
   session({
-    secret: "This will be secret",
+    secret: process.env.SESSION_SECRET || "fallbackSecret",
     resave: false,
     saveUninitialized: true,
     cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 1 day
   })
 );
 
+MONGODB_URI =
+  "mongodb+srv://your_username:your_password@cluster.mongodb.net/your_db?retryWrites=true&w=majority";
+
 mongoose
-  .connect(
-    "mongodb+srv://anxdidani:dominusoft@cluster0.oxrc6.mongodb.net/Final?retryWrites=true&w=majority&appName=Cluster0"
-  )
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("DB connected"))
-  .catch((err) => console.log("Something went wrong", err));
+  .catch((err) => {
+    console.log("Database connection error", err);
+    process.exit(1);
+  });
 
 app.use("/api/contacts", contactRoute);
 app.use("/api/products", productRoute);
@@ -51,18 +60,17 @@ app.use("/api/brands", brandRoute);
 app.use("/api/search", searchRoute);
 app.use("/api/user", userRoute);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
 
-  app.get("/", (req, res) => {
-    res.send("Backend is running! Go to /api/... for API routes.");
-  });
+//   app.get("/", (req, res) => {
+//     res.send("Backend is running! Go to /api/... for API routes.");
+//   });
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-  });
-}
-
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+//   });
+// }
 
 app.get("/message", (req, res) => {
   res.send("<h1>Hello Node!</h1>");
@@ -73,10 +81,11 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  console.error('Global error handler caught:', err);
-  res.status(500).json({ message: "Internal server error", error: err.message });
+  console.error("Global error handler caught:", err);
+  res
+    .status(500)
+    .json({ message: "Internal server error", error: err.message });
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
